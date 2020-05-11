65e74bcc77264a65d2fe9dadb9492010be7947ad
Eshitakalsi <eshitakalsi.ek@gmail.com>
2020-05-12 02:06:50 +0530

    Added Custom Data Model, Created custom plist, Encoding and Decoding done with NSCoder

---
+++ MiniList/Controllers/TodoListViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  MiniList
+//
+//  Created by Eshita Kalsi on 09/05/20.
+//  Copyright © 2020 Eshita Kalsi. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+    
+    
+    var itemArray = [Item]()
+    //    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadItems()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        //when "+" is pressed
+        let alert = UIAlertController(title: "Add new MiniList item", message: "", preferredStyle: .alert)
+        
+        //gets triggered when "Add Item" is pressed
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            self.saveItems()
+        }
+        //gets triggered once the text feild shows up in alert box
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding into array, \(error)")
+            }
+        }
+    }
+    
+    
+    
+}
+
+
+
---
+++ MiniList/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  MiniList
+//
+//  Created by Eshita Kalsi on 11/05/20.
+//  Copyright © 2020 Eshita Kalsi. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
